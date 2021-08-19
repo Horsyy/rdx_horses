0e5e847ee0458fdec87bf59c6205ad90f9124beb
Horse <42266290+Horsyy@users.noreply.github.com>
2021-08-19 11:18:33 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,11 +2,11 @@ Config = {}
 
 -- Percentage Health will be filled
 Config.WildCarrot	= 15
-Config.Apple		= 30
-Config.Corn			= 20
+Config.Apple		  = 30
+Config.Corn			  = 20
 Config.Ginseng		= 25
-Config.Beets		= 25
-Config.Peach		= 30
+Config.Beet			  = 25
+Config.Peach		  = 30
 
 Config.Syringe		= 100
 Config.Ointment		= 100
